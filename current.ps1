912dda06948abf9d8915629519f9bbfc738ae922
christoph hagenbrock <christoph.hagenbrock@googlemail.com>
2024-03-14 18:25:43 +0100

    fix ps1 bug in install script

--- current.ps1
+++ current.ps1
@@ -1,8 +1,3 @@
-#!/bin/bash
-
-# Set the environment variables
-export TS_NODE_PATH="/mnt/d/tmp/cmd/node_modules/ts-node"
-export TS_NODE_ROOT="/mnt/d/tmp/cmd"
-
-# Execute the node command
-node --inspect --experimental-specifier-resolution=node --import 'data:text/javascript, import { register } from "node:module"; import { pathToFileURL } from "node:url";  register("ts-node/esm", pathToFileURL(process.env.TS_NODE_PATH));' "$@"
+$env:TS_NODE_PATH = "D:\source\cmd\node_modules\ts-node";
+$env:TS_NODE_ROOT = "D:\source\cmd";
+node --experimental-specifier-resolution=node --import 'data:text/javascript, import { register } from "node:module"; import { pathToFileURL } from "node:url";  register("ts-node/esm", pathToFileURL(process.env.TS_NODE_PATH));' $args
--- install.js
+++ install.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 
 if(os.platform() === 'win32') {
     fs.writeFileSync('./current.ps1',fs.readFileSync('script.ps1', 'utf8').replaceAll('{dirname}', __dirname));
+    fs.writeFileSync('../../../_ts.ps1',fs.readFileSync('script.ps1', 'utf8').replaceAll('{dirname}', __dirname));
 } else {
     fs.writeFileSync('./current.ps1',fs.readFileSync('script.sh', 'utf8').replaceAll('{dirname}', __dirname));
 }
